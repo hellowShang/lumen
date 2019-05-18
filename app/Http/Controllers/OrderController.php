99489fe7f56d93381c60e01c22a1d1cb4a92f701
hellowShang <13473136348@163.com>
2019-05-18 12:13:29 +0000

    debug

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,10 +8,9 @@ class OrderController extends Controller
     // 订单生成
     public function orderGenerate(){
         $goods_id = $_GET['goods_id'];
-        $pay_way = $_GET['pay_way'];
         $uid = $_GET['id'];
         $url = env('HTTP_PATH').'/orderGenerate';
-        $response = curlGet($url,['goods_id' => $goods_id,'uid' => $uid,'pay_way' => $pay_way]);
+        $response = curlGet($url,['goods_id' => $goods_id,'uid' => $uid]);
         echo $response;
     }
 }
